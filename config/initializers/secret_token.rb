3551912c08d92e40dd4f64f08d98153179f90c8f
Mango <mango@Mangos-MacBook-Pro.local>
2014-01-29 09:09:47 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gaikokusan::Application.config.secret_key_base = '23257deb3b82c70a5eeb737e4798b7dacfab903308c5e33da360ea6f679078e18b298c057cc31fc66c6bf0850e0636232a555831df78e42aa08c650248711bb7'
